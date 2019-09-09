11addf6ab1c1662399372b3ab12d8cb7414100b5
zk-1999 <120362753@qq.com>
2019-09-09 08:45:06 +0800

    更改通用的通用的访问地址

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ module.exports = {
     devServer: {
         port: 8080, // 端口号
         // host: '192.168.31.147',
-        host: 'localhost',
-
+        // host: 'localhost',
+        host: '0.0.0.0',
         https: false, // https:{type:Boolean}
         open: true, //配置自动启动浏览器
         // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
